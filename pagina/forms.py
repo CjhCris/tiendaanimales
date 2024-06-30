6074bad238d75ed09e614e64960003309d52d808
ianvar18 <bryanvargas183@gmail.com>
2024-06-30 01:21:16 -0400

    base de datos

--- pagina/forms.py
+++ pagina/forms.py
@@ -3,7 +3,21 @@ from django import forms
 from .models import Usuario
 
 class UsuarioForm(forms.ModelForm):
+    confirmar_contraseña = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = Usuario
-        fields= ["usuario", "nombre", "apellido", "correo", "contraseña", "confirmar_contraseña", "rut", "edad"]
+        fields= ["usuario", "nombre", "apPaterno", "correo", "contraseña", "confirmar_contraseña", "rut", "edad"]
+        widgets = {
+            'contraseña': forms.PasswordInput(),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        contraseña = cleaned_data.get("contraseña")
+        confirmar_contraseña = cleaned_data.get("confirmar_contraseña")
+
+        if contraseña and confirmar_contraseña and contraseña != confirmar_contraseña:
+            self.add_error('confirmar_contraseña', "Las contraseñas no coinciden")
+
+        return cleaned_data
